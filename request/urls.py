084b49e426fced7da5b6e8df65e1197c1ac75cbc
solaris-9 <solariz9@hotmail.com>
2024-09-11 08:48:53 +0800

    2nd

---
+++ request/urls.py
@@ -0,0 +1,42 @@
+"""Resource URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from allocate import views as alloc_views
+from user import views as user_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('request/gpi/user/login', user_views.login),
+    path('request/gpi/user/info', user_views.info),
+    path('request/gpi/user/logout', user_views.logout),
+    path('request/gpi/user/user_manage', user_views.user_manage),
+    path('request/gpi/user/user_edit', user_views.user_edit),
+    path('request/gpi/allocate/customerid', alloc_views.customerid),
+    path('request/gpi/allocate/customer_list', alloc_views.customer_list),
+    path('request/gpi/allocate/customerlist', alloc_views.customerlist),
+    path('request/gpi/allocate/customer_list', alloc_views.customer_list),
+    path('request/gpi/allocate/request_info', alloc_views.request_info),
+    path('request/gpi/allocate/request_edit', alloc_views.request_edit),
+    path('request/gpi/allocate/new_boeng_info', alloc_views.new_boeng_info),
+    path('request/gpi/allocate/new_boeng_edit', alloc_views.new_boeng_edit),
+    path('request/gpi/allocate/fetch_boengrule', alloc_views.fetch_boengrule),
+    path('request/gpi/allocate/customer_id', alloc_views.customer_id),
+    path('request/gpi/allocate/customer_id_edit', alloc_views.customer_id_edit),
+    path('request/gpi/allocate/customerid', alloc_views.customerid),
+    path('request/gpi/allocate/devicelist', alloc_views.devicelist),
+]
---
+++ user/views.py
@@ -0,0 +1,283 @@
+from django.shortcuts import render
+
+# Create your views here.
+import datetime
+import simplejson
+import json
+import ldap
+# import os
+
+from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from utils import analyzer_db, create_token
+File_address = r'C:/reqLog/printlog.txt'
+fa = open(File_address,'a')
+# LDAP_HOST = 'ldap://10.152.138.3:389' 
+LDAP_HOST = 'ldap://10.158.52.11:389' 
+LDAP_BASE_DN = 'OU=Users,OU=UserAccounts,DC=nsn-intra,DC=net'
+
+def ldap_auth(username, password):
+  try:
+       
+    conn = ldap.initialize(LDAP_HOST)        
+    conn.simple_bind_s('nsn-intra\\' + username, password)
+    result = conn.search_s(LDAP_BASE_DN, ldap.SCOPE_SUBTREE, 'sAMAccountName=' + username)    
+    result = result[0][1]
+    user_info = {
+      'full_name': result['cn'][0].decode('utf-8'),
+      'f_name': result['givenName'][0].decode('utf-8'),
+      'l_name': result['sn'][0].decode('utf-8'),
+      'mail': result['mail'][0].decode('utf-8')}
+    return user_info
+   
+  except Exception as e:
+    print('--> ldap_auth Err:', e)
+    return False
+
+
+@csrf_exempt
+def login(request): 
+  sLastupdate = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')  
+  if 'HTTP_X_FORWARDED_FOR' in request.META:
+    print('--> rocklog login ip:', sLastupdate,request.META.get('HTTP_X_FORWARDED_FOR'),file=fa,flush=True )
+  else:
+    print('--> rocklog login ip:', sLastupdate,request.META.get('REMOTE_ADDR'),file=fa,flush=True )
+  print('request.body:', request.body.decode('utf-8'),file=fa,flush=True )
+  info = json.loads(request.body.decode('utf-8'))
+  username = info['username']
+  password = info['password'] 
+  ldap_user = ldap_auth(username, password)  
+  token, exp_time = create_token(username)
+  
+  print(sLastupdate,ldap_user, file=fa,flush=True )
+  if ldap_user:
+    print('user1-1=', sLastupdate, username, file=fa, flush=True)
+    sql = analyzer_db()
+    print('user1=',sLastupdate,username, file=fa,flush=True )
+    old_user = sql.search_user(key=username)
+    print('user2=',sLastupdate,username, file=fa,flush=True )
+    if old_user:
+      print('user3=', sLastupdate, username, file=fa, flush=True)
+      user = sql.update_user(
+        old_user['id'], ldap_user['full_name'], ldap_user['f_name'],
+        ldap_user['l_name'], ldap_user['mail'], token, exp_time,sLastupdate)
+      roles = old_user['roles']
+      level = old_user['level']  
+      # print(roles,level,ldap_user['mail'],file=fa,flush=True )
+    else:
+      user = sql.insert_user(
+        username, ldap_user['mail'], token,
+        f_name = ldap_user['f_name'],
+        l_name = ldap_user['l_name'],
+        full_name = ldap_user['full_name'],
+        roles = 'viewer',
+        level = '1',
+        exp_time = exp_time,
+        login_time = sLastupdate)
+      roles = 'views'
+      level = '1'
+        
+    log1 = sql.search_log(key1=username,key2='',key3='Login',key4=sLastupdate,key5='login success') 
+    if not log1:
+        log2=sql.insert_log(
+            username = username,
+            accweb='',
+            operation='login',
+            accdate=sLastupdate,
+            status ='login success'
+            )
+    sql.close()
+    data = {
+      'name': username,
+      'mail': ldap_user['mail'],      
+      'token': user['token'],      
+      'roles': roles,
+      'level': level
+    }
+    resp = {
+      'code': 20000,
+      'mes': 'login success',
+      'data': data
+    }    
+    return HttpResponse(json.dumps(resp), content_type='application/json')
+  else:
+      sql = analyzer_db()
+      print('user4=', sLastupdate, username, file=fa, flush=True)
+      log1 = sql.search_log(key1=username,key2='',key3='Login',key4=sLastupdate,key5='login failure') 
+      if not log1:
+          log2=sql.insert_log(
+              username = username,
+              accweb='',
+              operation='login',
+              accdate=sLastupdate,
+              status ='login failure'
+              )
+      data = {
+        'name': username        
+      }
+      resp = {
+        'code': 20000,
+        'mes': 'login failure,username or password error ',
+        'data': data
+      } 
+      return HttpResponse(json.dumps(resp), content_type='application/json')
+
+
+@csrf_exempt
+def info(request):
+  token = request.GET.get('token', None)  
+  sql = analyzer_db()
+  user_info = sql.search_user(key=token)
+  sql.close()
+  if user_info:
+    data = {
+      'name': user_info['username'],
+      'avatar': 'http://135.251.207.221/images/avatar.gif',
+      'mail': user_info['mail'],
+      'roles': user_info['roles'],
+      'level': user_info['level']
+    }
+    resp = {
+      'code': 20000,
+      'mes': 'get user info success',
+      'data': data
+    }
+    return HttpResponse(json.dumps(resp), content_type='application/json')
+  else:
+    return HttpResponse('token error', status=405)
+
+
+@csrf_exempt
+def logout(request):
+  # token = request.COOKIES['encodedAuth']
+  username = request.COOKIES['username'] 
+  sql = analyzer_db()  
+  sLastupdate = datetime.datetime.today().strftime('%Y-%m-%d')
+  log1 = sql.search_log(key1=username,key2='',key3='Logout',key4=sLastupdate,key5='logout success') 
+  if not log1:
+    log2=sql.insert_log(
+        username = username,
+        accweb='',
+        operation='logout',
+        accdate=sLastupdate,
+        status ='logout success'
+        )
+  print('name=',username,file=fa,flush=True )  
+  sql.close()
+  # sql.delete_user(Id=user_info['id'])
+  resp = {
+    'code': 20000,
+    'mes': 'Logout success',
+  }
+  return HttpResponse(json.dumps(resp), content_type='application/json')
+
+def user_manage(request):
+    dResult = {}
+    dResult['code'] = 20000
+    dResult['data'] = {}
+    dResult['data']['status'] = []
+    dResult['data']['items'] = []
+    try:
+        if request.method == 'POST':
+            data = json.loads(request.body)
+            if data:
+                sUsername = data.get('username')
+        else:
+            sUsername = request.GET['username']
+    except:
+        dResult['data']['status'] = "Invalid Parameters"
+        return HttpResponse(simplejson.dumps(dResult), content_type='application/json')
+
+    cmd = """
+        SELECT
+            ID,Username, Email, Roles, Level, FirstName, LastName, FullName, LastLogin
+        FROM
+            auth_user
+            %s
+            
+    """
+    if sUsername == 'all':       
+        sRule = ''
+    else:
+        sRule = "WHERE Username = '%s'" % sUsername       
+        
+    SQLConn = analyzer_db() 
+    SQLConn.cur.execute(cmd % sRule)
+    SQLResult = SQLConn.cur.fetchall()
+    SQLConn.close()
+        
+    for row in SQLResult:        
+        dItem = {}
+        dItem['ID'] = row[0]        
+        dItem['Username'] = row[1]
+        dItem['Email'] = row[2]
+        dItem['Roles'] = row[3]
+        dItem['Level'] = row[4]        
+        dItem['FirstName'] = row[5]
+        dItem['LastName'] = row[6]
+        dItem['FullName'] = row[7]
+        dItem['LastLogin'] = str(row[8])
+        dResult['data']['items'].append(dItem)
+    
+    return HttpResponse(simplejson.dumps(dResult), content_type='application/json')
+
+def user_edit(request):
+    
+    try:
+        sType = request.GET['type'] 
+        sLastupdate = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+        if sType == '1':
+            sUsername = request.GET['Username']  
+            sMail = request.GET['Mail'] 
+            sLevel = request.GET['Level'] 
+            sGrade = request.GET['Roles']            
+        elif sType == '2':
+            sUSID = request.GET['ID']
+            sMail = request.GET['Mail']
+            sLevel = request.GET['Level'] 
+            sGrade = request.GET['Roles'] 
+                        
+    except:
+        return HttpResponse('Invalid Parameters', content_type='application/json')
+    
+    dResult = {}
+    dResult['code'] = 20000
+    dResult['data'] = {}
+    dResult['data']['items'] = []    
+    Result ='' 
+    
+    # 1 add
+    if sType == '1': 
+        token, exp_time = create_token(sUsername)
+        sql = analyzer_db()    
+        old_user = sql.search_user(key=sUsername) 
+        if old_user:           
+            Result = 'The user is exist, do not add again.'    
+        else:          
+          user = sql.insert_user(
+            sUsername, sMail, token,
+            f_name ='',
+            l_name ='',
+            full_name ='',
+            roles = sGrade,
+            level = sLevel,
+            exp_time = exp_time,
+            login_time = sLastupdate)
+          Result = 'Add user successful' 
+        sql.close()   
+    # 2 edit
+    elif sType == '2': 
+        sql = analyzer_db()        
+        cmd="update auth_user set  Email ='%s', Level= '%s', Roles= '%s' where ID =  '%s'" % (sMail, sLevel, sGrade, sUSID) 
+        
+        sql.cur.execute(cmd)
+        sql.conn.commit()
+        Result = 'Modify user successful' 
+        sql.close()
+    dItem = {}
+    dItem['Result'] = Result                
+    dResult['data']['items'].append(dItem)
+    return HttpResponse(simplejson.dumps(dResult), content_type='application/json')
+
+
+
